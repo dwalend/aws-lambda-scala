00e43c866d673acebd43978dc10c738921c3dbe7
dwalend <david@walend.net>
2016-04-07 17:31:22 -0400

    Added a package.scala with some roughed-out URLs

---
+++ lambda/src/main/scala/net/walend/lambdademo/lambda/package.scala
@@ -0,0 +1,22 @@
+package net.walend.lambdademo
+
+/**
+  * Web API
+  *
+  * /user/ID/addEdge/ID POST - add a new relationship and post an edge . Body is some words about the relationship.
+  * /users  GET - return a list of all users
+  * /user/ID/edges GET - return a list of all user ID -> ID relationships
+  * /user/ID/noEdge GET - return a list of all IDs with no relationship to the user
+  * /edges GET - return a list of all relationships
+  *
+  *
+  * 634303334930 - AWS account number
+  *
+  * https://634303334930.signin.aws.amazon.com/console/
+  *
+  * @author dwalend
+  * @since v0.0.0
+  */
+package object lambda {
+
+}
